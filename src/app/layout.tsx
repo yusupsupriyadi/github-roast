5bc1ad219116a59317ba971fef3ad3f48e34e0a0
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-04 16:49:52 +0700

    feat(layout): add Umami analytics script to RootLayout component
    - Integrated the Umami analytics script into the RootLayout component for enhanced tracking and analytics capabilities.
    - This addition allows for better insights into user interactions and application performance.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -19,6 +20,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<Script
+					defer
+					src='https://cloud.umami.is/script.js'
+					data-website-id='5a6f226f-11ab-447d-9279-cbad080f6db2'></Script>
+			</head>
 			<body className={`${inter.variable} font-sans antialiased`}>
 				{children}
 			</body>
