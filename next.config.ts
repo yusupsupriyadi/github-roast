b1a835e9c97a074f1fe2364cc06e5f47457542c9
Yusup Supriyadi <yusupsupriyadistudio@gmail.com>
2024-12-05 10:56:12 +0700

    feat(next.config): enhance image handling and security settings
    - Added remote patterns for loading images from the Product Hunt API.
    - Enabled SVG support and set content disposition type to 'attachment'.
    - Implemented a content security policy to improve application security.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'api.producthunt.com',
+				pathname: '/widgets/embed-image/**',
+			},
+		],
+		dangerouslyAllowSVG: true,
+		contentDispositionType: 'attachment',
+		contentSecurityPolicy:
+			"default-src 'self'; script-src 'none'; sandbox;",
+	},
 };
 
 export default nextConfig;
