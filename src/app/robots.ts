d3927ef6e525dca7edf9941392d63ec8f8b61b38
yusupsupriyadi <yusupsupriyadistudio@gmail.com>
2025-01-24 15:48:46 +0700

    refactor(robots): seo optimaze

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,12 @@
 export default function robots() {
 	return {
+		rules: [
+			{
+				userAgent: '*',
+				allow: '/',
+				crawlDelay: 300,
+			},
+		],
 		sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
 	};
 }
